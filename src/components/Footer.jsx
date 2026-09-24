7824581a61e140810a987d7183755c7e6bcad74e
creonexviz <creonexviz@gmail.com>
2026-09-24 17:52:42 +0530

    feat: update footer track order label to 'Track Order with Booked Mobile #'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -73,7 +73,7 @@ const Footer = () => {
             <li>
               <Link to="/track-order" className="footer__link" style={{ color: 'var(--color-gold)', fontWeight: 600 }}>
                 <Phone size={15} />
-                <span>Track Order by Phone #</span>
+                <span>Track Order with Booked Mobile #</span>
               </Link>
             </li>
             <li>
